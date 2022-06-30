43de7c895cca8ec1439d97c8d62d970fe2648c0b
Ellie <elliepark1012@gmail.com>
2022-06-30 17:41:42 -0400

    commit tree 9ffde55025f70902f622466fb7ca1807c8c8b5bb

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) +1; 
+
+let num3 = 12
+let num4 = 3
+const mod = num3/num4
+
+const max = 
+Math.max(1,2,3,4,5,6,7,8,9,10,11,12,20);
